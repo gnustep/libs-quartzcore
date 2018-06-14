4fc0a6249c0941a829613074078a7ba7a991233e
Stjepan Brkić <stjepanbrkicc@gmail.com>
2018-06-15 00:17:34 +0200

    Fix main (demo)

--- CAAppKitBridge/Demo/main.m
+++ CAAppKitBridge/Demo/main.m
@@ -24,7 +24,7 @@
    Boston, MA 02110-1301, USA.
 */
 
-#import "../Headers/CAAppKitBridge/CAData.h"
+#import "../Source/GSCAData.h"
 #import "../Source/NSView+CAMethods.h"
 #import <Foundation/Foundation.h>
 
